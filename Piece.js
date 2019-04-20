65c3ed11c78f3394b288ff1d41d803f4b65fa540
nomiskeig <nomiskeig@gmail.com>
2019-04-20 21:38:44 +0200

    Kein Feststecken an den Seiten, Linien entfernt

--- Piece.js
+++ Piece.js
@@ -99,7 +99,8 @@ class Piece {
   rotatePiece() {
 
     this.createRotatedIndex();
-
+    if (this.x == 8*scale) this.x -= scale;
+    if (this.x == -scale) this.x += scale;
 
     for (let i = 0; i <= this.newPieceIndex.length - 1; i++) {
       for (let j = 0; j <= this.newPieceIndex[i].length - 1; j++) {
@@ -142,31 +143,6 @@ class Piece {
   }
 
   movePieceToBottom() {
-
-    // let distance = ["0", "0", "0"];
-    // let minDistance = 0;
-    // let differenceTopToPiece = Math.floor(this.y/scale) + this.getPieceHeight()/scale;
-    // console.log("differenceTopToPiece = " + differenceTopToPiece)
-    // for (let i = 0; i <= 2; i++) {
-    //   for (let j = 0; j <= (28 - differenceTopToPiece); j++) {
-    //     if (mainIndex.index[this.x / scale + i][differenceTopToPiece + j] == 0) {
-    //       distance[i]++;
-    //       console.log("Distanz = " + distance[i]);
-    //     }
-    //   }
-    // }
-    // minDistance = Math.min(...distance);
-    // console.log("minDistance= " + minDistance)
-    // for (let i = 0; i <= this.pieceIndex.length - 1; i++) {
-    //   for (let j = 0; j <= this.pieceIndex[i].length - 1; j++) {
-    //     if (this.piece[i][j]) {
-    //       console.log("gemovt");
-    //       this.piece[i][j].moveBoxToBottom(minDistance*scale);
-
-    //     }
-    //   }
-
-    // }
 
     while (this.checkNoContact(floor) && this.checkNoCollisionBot()) {
       for (let i = 0; i <= this.pieceIndex.length - 1; i++) {
